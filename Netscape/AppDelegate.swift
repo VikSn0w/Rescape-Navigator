4dc49e11784089b4402e70534756702d1c9313c8
VIK <vittoriopicone30@gmail.com>
2019-05-12 18:39:44 +0200

    Initial Commit

---
+++ Netscape/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Netscape
+//
+//  Created by Vittorio Picone on 12/05/2019.
+//  Copyright © 2019 viksn0w. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
